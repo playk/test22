fd710cc608e3ed9e2b79226e45256725efe751a4
Paul Trounov <p.l.a.y.k@yandex.ru>
2014-03-07 10:19:47 +0400

    comments

--- bubble.cpp
+++ bubble.cpp
@@ -1,6 +1,11 @@
 #include<stdlib.h>
 #include <stdio.h>
 
+/**
+@brief Функция сортировки
+@detailed Осуществляет сортировку массива символов методом пузырька
+@params Массив символов, размер массива
+*/
 void bubble(char* a, int n)
 {
   for (int i=n-1; i>=0; i--)
--- main.cpp
+++ main.cpp
@@ -9,23 +9,36 @@ using namespace std;
 
 void main()
 {
+	/**
+	@brief инициализация массиов символов
+	*/
 	char mass[10];
 	char massm[10];
+	/**
+	@brief ввод массива для сортировки пузырьком
+	*/
 	for(int id=0; id<10; id++)
 	{
 	cin>>mass[id];
 	}
+	/**
+	@brief вызов функции сортировки пузырьком
+	*/
 	bubble(mass,10);
 		for(int id=0; id<10; id++)
 	{
 	cout<<mass[id];
 	}
-
+    /**
+    @brief ввод массива для сортировки слиянием
+    */
 for(int id=0; id<10; id++)
 	{
 	cin>>massm[id];
 	}
-
+    /**
+	@brief вызов функции сортировки слиянием
+	*/
 	merge_sort(mass,0,9);
 for(int id=0; id<10; id++)
 	{
